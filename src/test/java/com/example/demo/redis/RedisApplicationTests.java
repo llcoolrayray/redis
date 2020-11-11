999399418748e43577644772f5236c15010f924e
754165369@qq.com <754165369@qq.com>
2020-11-11 22:33:42 +0800

    [feat]: spring boot 配置redis

--- src/test/java/com/example/demo/redis/RedisApplicationTests.java
+++ src/test/java/com/example/demo/redis/RedisApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.demo.redis;
 
+import com.example.demo.redis.util.RedisUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+
 @SpringBootTest
 class RedisApplicationTests {
+    @Resource
+    private RedisUtil redisUtil;
 
     @Test
     void contextLoads() {
+        redisUtil.set("name","wangrui");
+        System.out.println(redisUtil.get("name"));
     }
 
 }
